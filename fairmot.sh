ff7b1ee3e79fc04f1791494a171cfbcb7210a237
wc1229 <wengchuang2666@gmail.com>
2022-05-30 15:56:45 +0800

    fairmot onnx

--- fairmot.sh
+++
@@ -1,3 +0,0 @@
-#python3 detection/fairmot/fairmot_onnx_infer.py -i /home/lpj/wc/image/0.jpg -o /home/lpj/wc/FairMOT-master/exp/mot/mot17_yolo_17/model_last.onnx 
-#python3 detection/fairmot/torch_model.py --arch yolo --load_model '/home/lpj/wc/FairMOT-master/exp/mot/mot17_yolo_17/model_last.pth'
-python3 detection/fairmot/onnxrun.py -i /home/lpj/wc/image/0.jpg -o /home/lpj/wc/FairMOT-master/exp/mot/mot17_yolo_17/model_last.onnx --K 6
